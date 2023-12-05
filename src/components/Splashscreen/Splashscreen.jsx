ff4d0608a7d390fb6b62a600477dd2300732093d
isksariful <cessinideveloper@gmail.com>
2023-12-05 23:39:14 +0530

    new
    new logo adede

--- src/components/Splashscreen/Splashscreen.jsx
+++ src/components/Splashscreen/Splashscreen.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import './Splashscreen.css'; 
+import './Splashscreen.css';
+import mainlogo from '../../assets/ayatriologo.png';
 const Splashscreen = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-white">
       <div className="text-center">
-       
+
         <img
-          src="https://s3-symbol-logo.tradingview.com/logo-yazilim--600.png"
+          src={mainlogo}
           alt="Logo"
-          className="w-24 h-auto animate-pulse transition-all ease-linear duration-1000" 
+          className="w-80 h-auto animate-pulse transition-all ease-linear duration-1000"
         />
-        <h1 className="text-2xl mt-4 font-bold">AirBnb</h1>
       </div>
     </div>
   );
