cd4a4f5137d99acf875ecca37f7ad7fe687d2883
Ehsan <ctech1699@gmail.com>
2023-12-08 21:14:45 +0530

    magazineAdded

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from "./pages/HomePage";
 import MapPage from "./pages/MapPage";
 import ProductPage from "./pages/ProductPage";
-import Magazine from "./components/Magazine/Magazine";
+import Magazine from "./pages/MagazinePage";
 import RoomPage from "./pages/RoomPage";
+import MagazinePage from "./pages/MagazinePage";
 
 
 
@@ -17,7 +18,7 @@ function App() {
         <Route path="/" element={<HomePage/>} /> 
         <Route path="/ayatrio-map" element={<MapPage/>} /> 
         <Route path="/products" element={<ProductPage/>} />
-        <Route path="/magazine" element={<Magazine/>} />
+        <Route path="/magazine" element={<MagazinePage/>} />
         <Route path="/rooms" element={<RoomPage/>} />
       </Routes>
     </>
--- src/pages/MagazinePage.jsx
+++ src/pages/MagazinePage.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 import HTMLFlipBook from "react-pageflip";
 import "./Magazine.css";
+import Header from "../components/Header";
 
 const unsplashImages = [
   "https://images.unsplash.com/photo-1700143418116-d94c3f23b9ab?q=80&w=1370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -13,23 +14,35 @@ const unsplashImages = [
   "https://images.unsplash.com/photo-1701769007323-c7d360641ef8?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1701769007323-c7d360641ef8?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1701769007323-c7d360641ef8?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-
 ];
 
+const MagazinePage = () => {
+  const magazineRef = useRef(null);
 
-const Magazine = () => {
   const handlePrevPage = () => {
-    // Implement logic to handle previous page
+    magazineRef.current.pageFlip().flipPrev();
   };
 
   const handleNextPage = () => {
-    // Implement logic to handle next page
+    magazineRef.current.pageFlip().flipNext();
   };
 
-  return (
+  return (<>
+  <Header/>
     <div className="magazine-container">
       <div className="flip-book-container">
-        <HTMLFlipBook width={300} height={424} size="stretch" minWidth={315} maxWidth={1000} minHeight={420} maxHeight={1533} drawShadow={true} className="flip-book">
+        <HTMLFlipBook
+          ref={magazineRef}
+          width={300}
+          height={424}
+          size="stretch"
+          minWidth={315}
+          maxWidth={1000}
+          minHeight={420}
+          maxHeight={1533}
+          drawShadow={true}
+          className="flip-book"
+        >
           {unsplashImages.map((imageUrl, index) => (
             <div key={index}>
               <img src={imageUrl} alt={`Page ${index + 1}`} />
@@ -38,12 +51,22 @@ const Magazine = () => {
         </HTMLFlipBook>
       </div>
       <div className="pagination">
-        <button onClick={handlePrevPage}>&lt; Previous Page</button>
-        <span>Page 1 of {unsplashImages.length}</span>
-        <button onClick={handleNextPage}>Next Page &gt;</button>
+        <div
+          className="cursor-pointer px-4 py-2 bg-blue-500 text-white mr-2"
+          onClick={handlePrevPage}
+        >
+          &lt; Previous Page
+        </div>
+        <div
+          className="cursor-pointer px-4 py-2 bg-blue-500 text-white"
+          onClick={handleNextPage}
+        >
+          Next Page &gt;
+        </div>
       </div>
     </div>
+    </>
   );
 };
 
-export default Magazine;
+export default MagazinePage;
