69b468a8b05c72e877226611e9db8f8bbc0b0736
Ehsan <ctech1699@gmail.com>
2023-12-11 19:09:09 +0530

    Reverted  changes (Errors)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,21 +11,7 @@ import PopUp from '../components/PopUp/PopUp';
 
 const HomePage = () => {
   const navigate = useNavigate();
-<<<<<<< HEAD
-  const [loading, setLoading] = useState(true);
-  const [showPopup, setShowPopup] = useState(false);
-
-  useEffect(() => {
-    const hasPopupBeenShown = localStorage.getItem('popupShown');
-
-    if (!hasPopupBeenShown) {
-      setShowPopup(true);
-      localStorage.setItem('popupShown', 'true');
-    }
-  })
-=======
   const [isFilterVisible, setIsFilterVisible] = useState(true);
->>>>>>> 7c06079a3ac1bb63bab2472bd11d197ee7ec831d
 
   useEffect(() => {
     let prevScrollPos = window.scrollY;
@@ -47,26 +33,6 @@ const HomePage = () => {
 
   return (
     <>
-<<<<<<< HEAD
-      {loading ? (
-        <Splashscreen />
-      ) : (
-        <div className='fade-in'>
-          {showPopup && <PopUp />}
-          <Header />
-          <MobileSeachBar />
-          <Filter />
-          <Cards />
-          <button
-            type="button"
-            className="fixed left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 bottom-0 text-white bg-black hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-            onClick={() => navigate('/ayatrio-map')}
-          >
-            Show Map <Map className="ml-2" />
-          </button>
-        </div>
-      )}
-=======
       <div className={`fade-in ${isFilterVisible ? 'show-filter' : 'hide-filter'}`}>
         {popUp === 'true' ? null : <PopUp />}
         <Header />
@@ -81,7 +47,6 @@ const HomePage = () => {
           Show Map <Map className="ml-2" />
         </button>
       </div>
->>>>>>> 7c06079a3ac1bb63bab2472bd11d197ee7ec831d
     </>
   );
 };
