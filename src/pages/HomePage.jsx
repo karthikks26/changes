a02cd71c0797030dbcb93e8f1f55f928c4b0c9a3
isksariful <cessinideveloper@gmail.com>
2023-12-18 00:58:13 +0530

    name changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -69,10 +69,10 @@ const HomePage = () => {
         <Cards />
         <button
           type="button"
-          className="fixed left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 bottom-0 text-white bg-black hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          className="fixed left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 flex bottom-0 text-white bg-black hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
           onClick={() => navigate('/ayatrio-map')}
         >
-          Show Map  <img src={ayatrio_store} alt="" className="seachbar-div2-icon" />
+          Near Ayotrio <img src={ayatrio_store} alt="" className="seachbar-div2-icon" />
         </button>
       </div>
     </>
