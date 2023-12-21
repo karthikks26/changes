1ca5a35beaa1e3794a4dfb7a42df3e5c63f84cf0
isksariful <cessinideveloper@gmail.com>
2023-12-22 04:15:04 +0530

    login ui changes

--- src/components/Login/LoginComponent.jsx
+++ src/components/Login/LoginComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Google } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
+import mainlogo from "../../assets/ayatriologo.png";
 import axios from "axios";
 
 const LoginComponent = () => {
@@ -8,22 +9,45 @@ const LoginComponent = () => {
 
   const handleGoogleLogin = async () => {
     try {
-      window.open("http://localhost:8080/auth/google/callback","_self")
-    } catch (error) {}
+      window.open("http://localhost:8080/auth/google/callback", "_self")
+    } catch (error) { }
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-slate-100">
-      <div className="bg-white p-8 rounded-lg shadow-2xl w-96">
-        <h2 className="text-2xl font-semibold mb-4 text-center">
-          Login with Google
-        </h2>
+    <div className="flex items-center transform translate-y-14 justify-center">
+
+
+
+
+      <div className="bg-white">
+        <div className='img__con111'>
+          <img src={mainlogo} alt="logo" className="w-48 m-2" />
+        </div>
+
+        <div style={{ margin: "0px auto 18px", width: "100%" }}>
+          <h3
+            style={{
+              color: "rgb(0, 0, 0)",
+              fontSize: "30px",
+              fontWeight: "600",
+              letterSpacing: "-1.2px",
+              fontFamily: "inherit",
+              wordBreak: "break-word",
+            }}
+          >
+            Wellcome To Ayatrio Family Profile
+          </h3>
+          <div className='Modal_subtitle'>
+            Become a member of Ayatrio Family today. It's free to join? Get the details.
+          </div>
+        </div>
         <button
           onClick={handleGoogleLogin}
-          className="bg-blue-500 text-white rounded py-2 px-4 flex items-center justify-center w-full">
+          className="bg-blue-500 text-white rounded-full py-2 px-4 flex items-center justify-center w-full">
           <Google className="mr-2" />
           Login with Google
         </button>
+
       </div>
     </div>
   );
