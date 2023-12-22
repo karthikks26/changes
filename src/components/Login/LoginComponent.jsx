32032a5d2bcdcf361b541dbb0b20e09caa20b2b7
isksariful <cessinideveloper@gmail.com>
2023-12-22 17:33:56 +0530

    update loginui

--- src/components/Login/LoginComponent.jsx
+++ src/components/Login/LoginComponent.jsx
@@ -24,7 +24,7 @@ const LoginComponent = () => {
           <img src={mainlogo} alt="logo" className="w-48 m-2" />
         </div>
 
-        <div style={{ margin: "0px auto 18px", width: "100%" }}>
+        <div style={{ margin: "30px auto 10px", width: "100%" }}>
           <h3
             style={{
               color: "rgb(0, 0, 0)",
@@ -37,13 +37,14 @@ const LoginComponent = () => {
           >
             Wellcome To Ayatrio Family Profile
           </h3>
-          <div className='Modal_subtitle'>
-            Become a member of Ayatrio Family today. It's free to join? Get the details.
-          </div>
+
+        </div>
+        <div className='Modal_subtitle' style={{ margin: "0px 0 38px", width: "60%" }} >
+          Become a member of Ayatrio Family today. It's free to join? Get the details.
         </div>
         <button
           onClick={handleGoogleLogin}
-          className="bg-blue-500 text-white rounded-full py-2 px-4 flex items-center justify-center w-full">
+          className="text-white bg-black text-lg rounded-full font-semibold py-3 px-4 flex items-center justify-center w-3/4">
           <Google className="mr-2" />
           Login with Google
         </button>
