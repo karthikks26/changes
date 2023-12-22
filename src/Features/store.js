38f5eb27a64504c8d821b53b45dd9134b7ac4b44
tahirshaikh85 <tahirshaikhrs7@gmail.com>
2023-12-22 23:15:06 +0530

    rtk update

--- src/Features/store.js
+++ src/Features/store.js
@@ -1,17 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import recommendationReducer from "./recommendation/recommendationSlice";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./Sagas/index";
+import sliderReducer from "./slider/sliderSlice";
+import { sliderApi } from "./slider/sliderApi";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
-import { configureStore } from '@reduxjs/toolkit';
-import recommendationReducer from './recommendation/recommendationSlice';
-import createSagaMiddleware from 'redux-saga';
-import rootSaga from './Sagas/index';
-import sliderReducer from './slider/sliderSlice';
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
+    [sliderApi.reducerPath]: sliderApi.reducer,
     recommendedProduct: recommendationReducer,
     slider: sliderReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware,sliderApi.middleware),
 });
 
 sagaMiddleware.run(rootSaga);
