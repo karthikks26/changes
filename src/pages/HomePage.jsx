aabc91a8a1d56e252965ca49bb34c745ef58b741
Ehsan <ctech1699@gmail.com>
2023-12-24 02:01:47 +0530

    fixed conflicts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -136,7 +136,7 @@ const HomePage = () => {
         )}
         <Filter />
         <Cards />
-        <div className="fixed-ayatrio-map">
+        {isFilterVisible&&<div className="fixed-ayatrio-map">
           <button
             type="button"
             className="fixed left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 flex -bottom-3 text-white bg-black hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
@@ -149,7 +149,7 @@ const HomePage = () => {
               className="header-div-sStore-icon"
             />
           </button>
-        </div>
+        </div>}
         <div className="notch-buttons fixed z-50 py-2 bg-white w-full h-10  bottom-0 flex flex-row justify-evenly">
           <img src={home} alt="" className=" w-6 h-6" />
           
