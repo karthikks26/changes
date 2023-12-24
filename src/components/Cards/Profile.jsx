41d5de74a00bf491f24c02fac82584dfff749c5e
Harshi Agrawal <114180714+harshi706@users.noreply.github.com>
2023-12-25 03:16:10 +0530

    Update Profile.jsx

--- src/components/Cards/Profile.jsx
+++ src/components/Cards/Profile.jsx
@@ -1,42 +1,32 @@
 import React from 'react'
-import linkdln from './linkdln.jpg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import ProfileContent from './ProfileContent';
 
 const Profile = () => {
   return (
     <div>
-    <div className='bg-zinc-100'>
-      <div className='grid grid-cols-4'>
-        <div className='col-span-1 ml-12 mt-16'>
-          <div className='mb-2'>Inspiration to installation</div>
-          <div className='font-bold text-3xl mb-4'>Meet our designers, to make your dream to true</div>
-          <div className='text-xl mb-8'>We bring your vision to life with free white glove services</div>
-          <button className='bg-black pt-2 pb-2 pl-4 pr-4 mb-24 rounded-full text-white'>Book an appointment</button>
+      <div className='bg-zinc-100'>
+        <div class="grid grid-cols-4 gap-4">
+          <div class="col-span-1">
+            <div className='col-span-1 ml-12 mt-16'>
+              <div className='mb-2'>Inspiration to installation</div>
+              <div className='font-bold text-3xl mb-4'>Meet our designers, to make your dream to true</div>
+              <div className='text-xl mb-8'>We bring your vision to life with free white glove services</div>
+              <button className='bg-black pt-2 pb-2 pl-4 pr-4 mb-24 rounded-full text-white'>Book an appointment</button>
+            </div>  </div>
+          <div class="col-span-3">
+            <ProfileContent />
+          </div>
         </div>
-        <div className='col-span-1'>
-        <div className="bg-black rounded-full h-40 w-40 mb-2 mt-32 ml-20"></div>
-        <div className='flex justify-center'>
-        <div className='text-3xl font-bold ml-12'>Rajat Agrawal</div>
-        <a href='#'>
-      <img className='h-10 w-10' src={linkdln}/>
-    </a></div>
-        </div>
-        <div className='col-span-1'>
-        <div className="bg-black rounded-full h-40 w-40 mb-2 mt-32 ml-12"></div>
-        <div className='flex justify-center'>
-        <div className='text-3xl font-bold ml-12'>Rajat Agrawal</div>
-        <a href='#'>      <img className='h-10 w-10' src={linkdln}/>
-
-    </a></div>        </div>
-        <div className='col-span-1'>
-        <div className="bg-black rounded-full h-40 w-40 mb-2 mt-32 ml-12"></div>
-        <div className='flex justify-center'>
-        <div className='text-3xl font-bold ml-12'>Rajat Agrawal</div>
-        <a href='#'>      <img className='h-10 w-10' src={linkdln}/>
-
-    </a></div>        </div>
       </div>
-      </div>
+
     </div>
+
   )
 }
 
